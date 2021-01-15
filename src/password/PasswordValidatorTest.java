d49532eea8f52967d6194b0882730f1cf82eccd2
KwaiNks <richmondnkansah@gmail.com>
2021-01-15 10:39:37 -0500

    password validator test

--- src/password/PasswordValidatorTest.java
+++ src/password/PasswordValidatorTest.java
@@ -1,5 +1,41 @@
 package src.password;
 
+import org.junit.Test;
+
 public class PasswordValidatorTest {
     
+    String password = " ";
+    String passwordValidator;
+
+    @Test
+    public void shouldReturnWeakIfLessThanEight(){
+        password = "Today";
+        if (password.matches(".{1,7}$")) {
+            passwordValidator = "weakPassword";
+        }
+        assertEquals("weakPassword", passwordValidator);
+    }
+
+    @Test
+    public void shouldReturnMediumIfGreaterOrEqualToEightButLessThanFifteenAndContainsStringAndInt(){
+       password = "Richmond@Today2021";
+        if ((password.matches("(?=.*[A-Z]).{8,14}$"))) {
+            passwordValidator = "mediumPassword";
+        }
+        assertEquals("mediumPassword", passwordValidator);
+    }
+
+    @Test
+    public void shouldReturnStrongIfGreaterThanOrEqualsFifteen(){
+        password = "@TomorrowJan2021!";
+        if (password.matches("(?=.*\\W+)(?=.*[A-Z]).{15,}")) {
+            passwordValidator = "StrongPassword";
+        }
+        assertEquals("StrongPassword", passwordValidator);
+    }
+
+
+
+
+
 }
